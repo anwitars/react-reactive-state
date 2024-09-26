82b6453459e3c3938ab36fdb6efdf971afe1fa09
anwitars <maksa22242@gmail.com>
2024-09-26 16:30:19 +0200

    fix: use useMemo to avoid unnecessary recomputation of setters

--- src/reactiveState.ts
+++ src/reactiveState.ts
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction, useState } from "react";
+import { Dispatch, SetStateAction, useMemo, useState } from "react";
 
 const capitalize = (s: string) => s.charAt(0).toUpperCase() + s.slice(1);
 
@@ -80,22 +80,28 @@ export function useReactiveState<T extends Record<string, any>>(
 ): ReactiveState<T> {
   const [state, setState] = useState(initialState);
 
-  const setters = Object.keys(initialState).reduce((acc, key) => {
-    const setterKey = `set${capitalize(key)}` as keyof Setters<T>;
+  const setters = useMemo(
+    () =>
+      Object.keys(initialState).reduce((acc, key) => {
+        const setterKey = `set${capitalize(key)}` as keyof Setters<T>;
 
-    const setter = <K extends keyof T>(value: K | ((prev: T[K]) => T[K])) => {
-      let newValue = value;
-      if (typeof value === "function") {
-        newValue = value(state[key]);
-      }
+        const setter = <K extends keyof T>(
+          value: K | ((prev: T[K]) => T[K]),
+        ) => {
+          let newValue = value;
+          if (typeof value === "function") {
+            newValue = value(state[key]);
+          }
 
-      setState((prev) => ({ ...prev, [key]: newValue }));
-    };
+          setState((prev) => ({ ...prev, [key]: newValue }));
+        };
 
-    acc[setterKey] = setter as Setters<T>[keyof Setters<T>];
+        acc[setterKey] = setter as Setters<T>[keyof Setters<T>];
 
-    return acc;
-  }, {} as Setters<T>);
+        return acc;
+      }, {} as Setters<T>),
+    [initialState],
+  );
 
   return { ...state, ...setters, getValues: () => state };
 }
